0abae2f636f1b798446b7ecfc6157748d477487d
Sergio <Sergio@PCSERGIUS>
2025-11-19 13:47:37 +0100

    ayuda2nowispersonal

--- Reto3OctavianSergioAdrian/src/Reto/Reto1.java
+++ Reto3OctavianSergioAdrian/src/Reto/Reto1.java
@@ -5,7 +5,7 @@ public class Reto1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(Funcion2.invertirFrase("hola buenas "));
-		System.out.println(Funcion2.palabraLarga("estaaaaaaaa23 que tal "));
+		System.out.println(Funcion2.palabraLarga("1 22 333 4444 55555"));
 		
 		
 	}
