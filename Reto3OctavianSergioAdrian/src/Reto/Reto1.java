c6f0df62b4247c6ccdf23d0d890ce0350e4754e1
Adrian Ruiz <Adrin@MSI-ADRIAN>
2025-11-19 13:59:21 +0100

    dtyjdt

--- Reto3OctavianSergioAdrian/src/Reto/Reto1.java
+++ Reto3OctavianSergioAdrian/src/Reto/Reto1.java
@@ -1,29 +1,8 @@
 package Reto;
-<<<<<<< HEAD
-import Reto.Funcion3;
-=======
-import Reto.Funcion2;
->>>>>>> branch 'main' of https://github.com/adrianruizz/Reto3OctavianSergioAdrian.git
+
 public class Reto1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-
-		System.out.println(Funcion3.numerosEnFrase("se3o 5amorr  tu7pu8"));
-		System.out.println(Funcion3.vocalesString("hola que tal estas abecedario"));
-		
-		
-=======
-		System.out.println(Funcion2.invertirFrase("hola buenas "));
-<<<<<<< HEAD
-		System.out.println(Funcion2.palabraLarga("estaaaaaaaa23 que tal "));
->>>>>>> branch 'main' of https://github.com/adrianruizz/Reto3OctavianSergioAdrian.git
-=======
-		System.out.println(Funcion2.palabraLarga("1 22 333 4444 55555"));
->>>>>>> branch 'main' of https://github.com/adrianruizz/Reto3OctavianSergioAdrian.git
-		
+	
 		
 	}
 
-}
