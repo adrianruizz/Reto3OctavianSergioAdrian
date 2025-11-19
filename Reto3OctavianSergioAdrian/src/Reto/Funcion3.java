bbc2f6545941279ed0b9f00adba3fc807483b0c8
Adrian Ruiz <Adrin@MSI-ADRIAN>
2025-11-19 13:44:58 +0100

    Merge branch 'main' of https://github.com/adrianruizz/Reto3OctavianSergioAdrian.git into main

--- Reto3OctavianSergioAdrian/src/Reto/Funcion3.java
+++ Reto3OctavianSergioAdrian/src/Reto/Funcion3.java
@@ -1,5 +1,52 @@
 package Reto;
 
+import java.util.Scanner;
+
 public class Funcion3 {
+
+	public static String numerosEnFrase(String digito) {
+
+		String nueva = "";
+		boolean primera = true;
+		for (int i = 0; i < digito.length(); i++) {
+			char c = digito.charAt(i);
+			if (Character.isDigit(c)) {
+				if (primera) {
+					nueva = nueva + c;
+					primera = false;
+				} else {
+					nueva = nueva + "," + c;
+				}
+
+			}
+		}
+		return nueva;
+
+	}
+
+	
+	
+	
+	
+	public static int vocalesString(String texto) {
+		int contador = 0;
+		int maxvocales = 0;
+		int minvocales = 0;
+		String textoMinusculas = texto.toLowerCase();
+		for (int i = 0; i < textoMinusculas.length(); i++) {
+			char c = textoMinusculas.charAt(i);
+			if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+				contador++;
+			}
+			for (int j = minvocales; j < maxvocales; j++) {
+
+				if (maxvocales >minvocales) {
+					return maxvocales;
+				}
+				
+			}
+		}
+		return maxvocales;
+	}
 
 }
--- Reto3OctavianSergioAdrian/src/Reto/Reto1.java
+++ Reto3OctavianSergioAdrian/src/Reto/Reto1.java
@@ -1,10 +1,16 @@
 package Reto;
-
+import Reto.Funcion3;
 public class Reto1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(Funcion3.numerosEnFrase("se3o 5amorr  tu7pu8"));
+		System.out.println(Funcion3.vocalesString("hola que tal estas abecedario"));
+		
+		
+		
+		
 	}
 
 }
