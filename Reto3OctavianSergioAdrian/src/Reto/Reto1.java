2bfbf6a63025ef62d9d956daceb298241514f23a
Sergio <Sergio@PCSERGIUS>
2025-11-19 13:44:28 +0100

    ayuda

--- Reto3OctavianSergioAdrian/src/Reto/Reto1.java
+++ Reto3OctavianSergioAdrian/src/Reto/Reto1.java
@@ -1,10 +1,13 @@
 package Reto;
-
+import Reto.Funcion2;
 public class Reto1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(Funcion2.invertirFrase("hola buenas "));
+		System.out.println(Funcion2.palabraLarga("estaaaaaaaa23 que tal "));
+		
+		
 	}
 
 }
